853863831c84b9f8815821da2b5752aa90ee2755
myovira <meetdurga123@gmail.com>
2024-12-17 11:11:26 +0530

    yet to complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Header from './Componant/Header'
+import HeroSession from './Componant/HeroSession'
+import Client from './Componant/Client'
+
+
 
 const App = () => {
+  
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <Header />
+    <HeroSession />
+    <Client />
+    
+    </>
   )
 }
 
